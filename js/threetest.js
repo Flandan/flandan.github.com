9d4b37ac0b77ecc4a38a825d3216ba0ecd95df65
Liam <Flandan@users.noreply.github.com>
2017-10-10 13:43:44 +0100

    Update threetest.js

--- js/threetest.js
+++ js/threetest.js
@@ -88,10 +88,10 @@
 
             var context = canvas.getContext('2d');
             var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-            gradient.addColorStop(0, 'rgba(0,0,0,1)'); // rgba(255,255,255,1)
-            gradient.addColorStop(0.2, 'rgba(128,128,128,1)'); // rgba(0,255,255,1)
-            gradient.addColorStop(0.4, 'rgba(200,200,200,1)'); // rgba(0,0,64,1)
-            gradient.addColorStop(1, 'rgba(255,255,255,1)'); // rgba(0,0,0,1)
+            gradient.addColorStop(0, 'rgba(255,255,255,1)'); // rgba(255,255,255,1)
+            gradient.addColorStop(0.2, 'rgba(200,200,200,1)'); // rgba(0,255,255,1)
+            gradient.addColorStop(0.4, 'rgba(128,128,128,1)'); // rgba(0,0,64,1)
+            gradient.addColorStop(1, 'rgba(0,0,0,1)'); // rgba(0,0,0,1)
 
             context.fillStyle = gradient;
             context.fillRect(0, 0, canvas.width, canvas.height);
